178a246e0a02d6d971136e8b3786c63b4ea98924
Iván Alejandro Marugán <hello@ialejandro.rocks>
2018-02-19 13:48:49 +0100

    New PUB KEY Java

--- filebot-compiler.sh
+++ filebot-compiler.sh
@@ -79,7 +79,7 @@ install_packages() {
 		echo "[${WARN}WARNING${NC}] oracle-java${JAVA_VERSION} aren't installed. Installing..." 
 		echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" > /etc/apt/sources.list.d/webupd8team-java.list
 		echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" >> /etc/apt/sources.list.d/webupd8team-java.list
-		apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
+		apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C2518248EEA14886
 		echo oracle-java${JAVA_VERSION}-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
 		apt-get -qq install --allow-unauthenticated oracle-java${JAVA_VERSION}-installer oracle-java${JAVA_VERSION}-set-default &>/dev/null
 	else
